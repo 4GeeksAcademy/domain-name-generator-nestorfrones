0d49d9ef3cae2e72c681d1471f06aeb11208f1b7
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2024-05-01 00:50:12 +0000

    mejorado

--- src/app.js
+++ src/app.js
@@ -7,19 +7,27 @@ window.onload = function() {
 
   function domainNameGenerator() {
     let pronouns = ["the", "our"];
-    let adjetives = ["great", "big"];
+    let adjectives = ["great", "big"];
     let nouns = ["jogger", "racoon"];
     let exts = [".com", ".es"];
 
-    let results = [];
-
-    for (let pronoun of pronouns) {
-      for (let adj of adjetives) {
-        for (let noun of nouns) {
-          for (let ext of exts) {
-            let resultado = `<p>${pronoun}${adj}${noun}${ext}</p>`;
-            results.push(resultado);
-            dominio.innerHTML += resultado;
+    // for (let pronoun of pronouns) {
+    //   for (let adj of adjetives) {
+    //     for (let noun of nouns) {
+    //       for (let ext of exts) {
+    //         let resultado = `<p>${pronoun}${adj}${noun}${ext}</p>`;
+    //         dominio.innerHTML += resultado;
+    //         console.log(resultado);
+    //       }
+    //     }
+    //   }
+    // }
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjectives.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          for (let l = 0; l < exts.length; l++) {
+            let resultado = `${pronouns[i]}${adjectives[j]}${nouns[k]}${exts[l]}`;
+            dominio.innerHTML += `<p>${resultado}</p>`;
             console.log(resultado);
           }
         }
